a4c721e388e32dedb35cdeee9e49eefd78b2048e
claugeometriadoser <barrosclau@gmail.com>
2026-01-27 21:02:50 -0300

    Create index.ts

---
+++ config/index.ts
@@ -0,0 +1,5 @@
+export const CALENDLY_URL =
+  process.env.NEXT_PUBLIC_CALENDLY_URL || "https://exemplo.com/agendar";
+
+export const WHATSAPP_NUMBER =
+  process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || "5511987545477";
