c6beb29515cd2b66a5b52bdb93a59ac01697b04b
claugeometriadoser <barrosclau@gmail.com>
2026-04-10 16:29:37 -0300

    Update page.tsx

--- app/engenhariapp/page.tsx
+++ app/engenhariapp/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Page() {
-  return <h1>FUNCIONOU LIMPO</h1>;
+  const [resultado, setResultado] = useState("");
+
+  function calcular() {
+    setResultado("ok");
+  }
+
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Engenharia dos Padrões</h1>
+
+      <button onClick={calcular}>
+        Ver meu padrão
+      </button>
+
+      {resultado && <p>{resultado}</p>}
+    </main>
+  );
 }
